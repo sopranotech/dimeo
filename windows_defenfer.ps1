07fa8d5d5aa3a28f3ef96fa59756a4ed8209ffbf
sopranotech <sopranotechmx@gmail.com>
2024-08-23 12:54:27 -0600

    Delete windows_defenfer.ps1

--- windows_defenfer.ps1
+++
@@ -1,7 +0,0 @@
-# Excluir todo el directorio System32 en Windows Defender
-
-# Ruta del directorio System32
-$system32Path = "$env:SystemRoot\System32\"
-
-# Agregar el directorio como exclusión en Windows Defender
-Add-MpPreference -ExclusionPath $system32Path
